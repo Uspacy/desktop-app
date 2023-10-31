e2c92e0bcfe9ae508bc21b48f78d5bf17c3211b0
Stas Raranetskyi <rar.stas@gmail.com>
2023-10-31 13:38:08 +0000

    save window bounds

--- src/utils/cluster.ts
+++ src/utils/cluster.ts
@@ -3,7 +3,8 @@ import isDev from 'electron-is-dev';
 import { getClusterByLocale } from './locale';
 
 export const getStartUrl = () => {
-	if (isDev) return 'https://stage1.staging.uspacy.tech';
+	// if (isDev) return 'https://stage1.staging.uspacy.tech';
+	if (isDev) return 'http://localhost:3002';
 	const cluster = getClusterByLocale();
 	return `https://auth.uspacy.${cluster}`;
 };
--- src/utils/window.ts
+++ src/utils/window.ts
@@ -1,21 +1,32 @@
-import { BrowserWindow, shell } from 'electron';
+import { BrowserWindow, Rectangle, shell } from 'electron';
 import isDev from 'electron-is-dev';
+import Store from 'electron-store';
+
+export const store = new Store({ name: 'windowSettings' });
 
 import { getUserAgent } from './userAgent';
 
 export const createWindow = () => {
+	const windowBounds = store.get('windowBounds') as Rectangle;
 	const mainWindow = new BrowserWindow({
 		icon: './assets/logo.png',
 		webPreferences: {
 			preload: `${__dirname}/preload.js`,
 			devTools: isDev,
 		},
+		width: windowBounds?.width,
+		height: windowBounds?.height,
+		x: windowBounds?.x,
+		y: windowBounds?.y,
 	});
 	mainWindow.webContents.userAgent = getUserAgent(mainWindow);
-	mainWindow.maximize();
+	if (!windowBounds) mainWindow.maximize();
 	mainWindow.webContents.setWindowOpenHandler((details) => {
 		shell.openExternal(details.url);
 		return { action: 'deny' };
+	});
+	mainWindow.on('close', () => {
+		store.set('windowBounds', mainWindow.getBounds());
 	});
 	return mainWindow;
 };
