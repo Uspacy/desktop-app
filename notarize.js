52a33bebd9e858216cb4a53a2c9205a97c7e26f5
Mykhailo Serbeniuk <outsourcing.you@gmail.com>
2024-04-27 02:25:18 +0300

    Create notarize.js

---
+++ notarize.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'com.uspacy.app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleApiKey: process.env.API_KEY_ID,
+    appleApiIssuer: process.env.API_KEY_ISSUER_ID,
+  });
+};
