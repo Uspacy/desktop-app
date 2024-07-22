1df9f210381dab2b94f29a325c994efa2f26b403
Stas Raranetskyi <rar.stas@gmail.com>
2024-07-22 20:36:02 +0100

    auth stage

--- src/utils/cluster.ts
+++ src/utils/cluster.ts
@@ -3,6 +3,7 @@ import isDev from 'electron-is-dev';
 import { getClusterByLocale } from './locale';
 
 export const getStartUrl = () => {
+	if (true) return 'https://stage1.staging.uspacy.tech';
 	if (isDev) return 'https://stage1.staging.uspacy.tech';
 	// if (isDev) return 'http://localhost:3000';
 	const cluster = getClusterByLocale();
