1b25561750a3c177f2af02d9049aab4a20d68a48
Stas Raranetskyi <rar.stas@gmail.com>
2024-08-19 15:09:39 +0100

    [3366] feat: implement logic for all push notifications (#8)

--- src/utils/cluster.ts
+++ src/utils/cluster.ts
@@ -3,8 +3,9 @@ import isDev from 'electron-is-dev';
 import { getClusterByLocale } from './locale';
 
 export const getStartUrl = () => {
-	if (isDev) return 'https://stage3.staging.uspacy.tech';
-	// if (isDev) return 'http://localhost:3002';
+	// if (true) return 'https://stage1.staging.uspacy.tech';
+	if (isDev) return 'https://stage1.staging.uspacy.tech';
+	// if (isDev) return 'http://localhost:3000';
 	const cluster = getClusterByLocale();
 	return `https://auth.uspacy.${cluster}`;
 };
